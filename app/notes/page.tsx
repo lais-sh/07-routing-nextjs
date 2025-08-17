4913515556138c269dbdb4f2ebfe4f26930c8216
lais-sh <lithshamok@gmail.com>
2025-08-17 21:08:35 +1000

    new

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,5 @@
 import { fetchNotes } from '@/lib/api';
-import NotesClient from './Notes.client';
+import NotesClient from './filter/[...slug]/Notes.client';
 
 export const dynamic = 'force-dynamic';
 
