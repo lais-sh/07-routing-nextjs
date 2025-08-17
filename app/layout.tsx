a353570132839d1042e73ee4c4d763df9224312e
lais-sh <lithshamok@gmail.com>
2025-08-17 17:50:21 +1000

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,29 +3,27 @@ import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import type { Metadata } from 'next';
-import type { ReactNode } from 'react';
 
 export const metadata: Metadata = {
   title: 'NoteHub',
   description: 'Manage your personal notes efficiently.',
 };
 
-type RootLayoutProps = {
-  children: ReactNode;
-  modal: ReactNode; 
-};
-
-export default function RootLayout({ children, modal }: RootLayoutProps) {
+export default function RootLayout({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
         <TanStackProvider>
           <Header />
-          <main>
-            {children}
-            {modal}
-          </main>
+          <main>{children}</main>
           <Footer />
+          {modal}
         </TanStackProvider>
       </body>
     </html>
